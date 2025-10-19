9955a0806c02d6bbf8bb73e753d6b21996d0300b
Mahek Dudhat <mahekm452@gmail.com>
2025-10-19 11:58:30 +0530

    Refactor ThemeContext to include toggle function

--- 09themeswitcher/src/contexts/theme.js
+++ 09themeswitcher/src/contexts/theme.js
@@ -2,8 +2,7 @@ import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
     themeMode: "light",
-    darkTheme: () => {},
-    lightTheme: () => {},
+    toggle: () => {}
 })
 
 export const ThemeProvider = ThemeContext.Provider
