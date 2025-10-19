cce86c1812f181e7aeacc9ccb0d3611380ad7bde
Mahek Dudhat <mahekm452@gmail.com>
2025-10-19 11:47:53 +0530

    Refactor theme switching logic in App component

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -8,12 +8,12 @@ import Card from './components/Card'
 function App() {
   const [themeMode, setThemeMode] = useState("light")
 
-  const lightTheme = () => {
-    setThemeMode("light")
-  }
-
-  const darkTheme = () => {
-    setThemeMode("dark")
+  const toggle = () =>{
+       if(themeMode === "light"){
+           setThemeMode("dark")
+       }else{
+           setThemeMode("light")
+       }
   }
 
   // actual change in theme
@@ -25,7 +25,7 @@ function App() {
   
 
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <ThemeProvider value={{themeMode, toggle}}>
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
