b8978d3db089f3816bab5188f29904878b32e2e5
Mahek Dudhat <mahekm452@gmail.com>
2025-10-19 12:00:18 +0530

    Update App.jsx

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -8,14 +8,13 @@ import Card from './components/Card'
 function App() {
   const [themeMode, setThemeMode] = useState("light")
 
-  const lightTheme = () => {
-    setThemeMode("light")
+  const toggle = () => {
+     if(themeMode === "light"){
+       setThemeMode("dark")
+     }else{
+        setThemeMode("light")
+     }
   }
-
-  const darkTheme = () => {
-    setThemeMode("dark")
-  }
-
   // actual change in theme
 
   useEffect(() => {
@@ -25,7 +24,7 @@ function App() {
   
 
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <ThemeProvider value={{themeMode, toggle}}>
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
